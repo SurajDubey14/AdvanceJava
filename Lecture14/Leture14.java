43ba31993378bcb057fad4fb4cbdf7e152d6cadc
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2024-02-05 22:48:44 +0530

    Add files via upload

---
+++ Lecture14/Leture14.java
@@ -0,0 +1,10 @@
+package Lecture14;
+import java.util.*;
+public class Leture14 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number :- ");
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
