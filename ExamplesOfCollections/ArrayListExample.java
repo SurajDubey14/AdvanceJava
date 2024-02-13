899964a6350b02ddff09c658d730be1ecaf7951e
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2024-02-14 01:45:41 +0530

    Add files via upload

---
+++ ExamplesOfCollections/ArrayListExample.java
@@ -0,0 +1,15 @@
+package ExamplesOfCollections;
+
+import java.util.*;
+import java.io.*;
+
+class student{
+    private String stdid,name;
+    private int age;
+}
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<student> stdlist= new ArrayList<student>();
+
+    }
+}
