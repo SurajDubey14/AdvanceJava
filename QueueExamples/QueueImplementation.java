a032ced9083878b124af752bba5fb50348c4056f
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2024-02-19 04:37:49 +0530

    Add files via upload

---
+++ QueueExamples/QueueImplementation.java
@@ -0,0 +1,20 @@
+/*-----Queue Implementation using Dequeue*/
+package QueueExamples;
+
+import java.util.*;
+
+class Cities{
+private String cities;
+//public static get(String ){
+//
+//}
+}
+public class QueueImplementation {
+    public static void main(String[] args) {
+        Queue<String> city= new ArrayDeque<String>();
+        city.offer("Noida");
+        city.offer("Noida");
+        city.offer("Noida");
+        city.offer("Noida");
+    }
+}
